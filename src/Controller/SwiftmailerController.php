74355296b2e0357480989e16bca645c9db76f30e
DrappEtienne <etienne.drapp@epsi.fr>
2020-01-31 16:40:44 +0100

    Envoie de mail Début configuration du contenu des mails

--- src/Controller/SwiftmailerController.php
+++ src/Controller/SwiftmailerController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CoursRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,21 +11,32 @@ class SwiftmailerController extends AbstractController
     /**
      * @Route("/swiftmailer", name="swiftmailer")
      */
-    public function sendEmail($name, \Swift_Mailer $mailer)
+    public function sendEmail( \Swift_Mailer $mailer, CoursRepository $coursRepository)
     {
-        $message = (new \Swift_Message('Hello Email'))
+        $receiver=$this->getUser()->getMail();
+
+        $message = (new \Swift_Message('Cours crée avec succès !'))
             ->setFrom('tutorathep@gmail.com')
-            ->setTo('drapp.etienne@gmail.com')
+            ->setTo($receiver)     #Envoie à celui qui vient de crée le cours
             ->setBody(
                 $this->renderView(
-                // templates/hello/email.txt.twig
-                    'swiftmailer/index.txt.twig',
-                    ['name' => $name]
+                    'swiftmailer/index.html.twig'
                 )
             )
         ;
         $mailer->send($message);
 
-        return $this->render("courses");
+        $message = (new \Swift_Message('Un cours vient d être crée'))
+            ->setFrom('tutorathep@gmail.com')
+            ->setTo('tutorathep@gmail.com')          #Envoie à la boite mail admin
+            ->setBody(
+                $this->renderView(
+                    'swiftmailer/noticeCreationCours.html.twig'
+                )
+            )
+        ;
+        $mailer->send($message);
+
+        return $this->redirectToRoute("courses");
     }
 }
